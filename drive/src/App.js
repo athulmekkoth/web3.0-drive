e25a767920e966bdd9c80633c850cba4d0812144
Athul Suresh <athulmekkoth21@gmail.com>
2023-05-17 22:58:16 +0530

    metamaskdone

--- drive/src/App.js
+++ drive/src/App.js
@@ -1,11 +1,58 @@
 import React from "react"
 import { useState,useEffect } from "react";
-import { ethers } from "ethers";
+
 import Upload from "../src/artifact/contracts/Upload.sol/Upload.json"
+import FileUpload from "./compoennts/FileUpload";
+const ethers = require("ethers")
 function App() {
+  const[account,setAccount]=useState("")
+  const[contract,setContract]=useState(null)
+  const[provider,setProvider]=useState(null)
+  const[modalopen,setModalOpen]=useState(false)
+  useEffect(()=>{
+    //read
+    const provider=new ethers.providers.Web3Provider(window.ethereum)
+
+    const  loadprovider=async()=>{
+      if(provider)
+      {
+        //if account chanegd instanlty update
+      window.ethereum.on("chainChanged",()=>{
+        window.location.reload()
+
+      })
+      window.ethereum.on("accountChanged",()=>{
+        window.locaation.reload();
+      })
+        await provider.send("eth_requestAccounts",[]);
+        const signer=provider.getSigner()
+        const address=await signer.getAddress();
+        setAccount(address)
+        let contractadress="0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0"
+
+        const contract= new ethers.Contract(
+          contractadress,Upload.abi,signer
+        )
+
+        console.log(contract)
+        setContract(contract)
+
+        setProvider(provider)
+      }else{
+        console.log("metamask not found")
+
+      }
+  
+    }
+    provider &&  loadprovider( )
+},[])
+
+
   return (
-    <div className="App">
-    
+    <div className="bg-black h-screen">
+<h1 className="text-white">TrustDrop</h1>
+
+<p className="text-white">Account :{account ? account:"not cnnecged"}</p>
     </div>
   );
 }
---
+++ drive/src/compoennts/Model.js
@@ -0,0 +1,7 @@
+import React from "react";
+export default function Model()
+{
+    return(
+        <h1>l</h1>
+    )
+}
